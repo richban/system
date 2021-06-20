8cdfdd31d7d0c2399531142ea7d85633f8b22663
richban <rbanyi@me.com>
2021-06-20 19:58:39 +0200

    Last Sync: $'\n'  M sync.sh

--- sync.sh
+++ sync.sh
@@ -5,7 +5,6 @@ gstatus=`git status --porcelain`
 if [ ${#gstatus} -ne 0 ]; then
 	git pull
 	git add --all
-	git commit -q -m "Last Sync: $gstatus"
+	git commit -q -m "Last Sync: $'\n' $gstatus"
 	git push
-	terminal-notifier -title '🚀' -message 'Dotfiles Updated'
 fi
