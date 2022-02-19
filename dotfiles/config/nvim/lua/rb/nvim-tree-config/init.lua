3a92aba5bcb7d7318e59bfd9cd5557783d116e2b
richban <rbanyi@me.com>
2022-02-19 23:42:15 +0100

    Update nvim-tree

--- dotfiles/config/nvim/lua/rb/nvim-tree-config/init.lua
+++ dotfiles/config/nvim/lua/rb/nvim-tree-config/init.lua
@@ -1,46 +1,116 @@
-local map = vim.api.nvim_set_keymap
+local key_map = vim.api.nvim_set_keymap
 
-local tree_cb = require'nvim-tree.config'.nvim_tree_callback
-
-vim.g.nvim_tree_indent_markers = 1
-vim.g.nvim_tree_quit_on_open = 0
-vim.g.nvim_tree_git_hl = 0
-vim.g.nvim_tree_highlight_opened_files = 1
-vim.g.nvim_tree_group_empty = 1
-vim.g.nvim_tree_show_icons = {git = 1, folders = 1, files = 1, folder_arrows = 1}
-
-local nvim_tree_bindings = {
-  {key = "a", cb = tree_cb("create")}, {key = "d", cb = tree_cb("remove")}, {key = "x", cb = tree_cb("cut")}, {key = "c", cb = tree_cb("copy")},
-  {key = "p", cb = tree_cb("paste")}, {key = "q", cb = tree_cb("close")}, {key = "-", cb = tree_cb("close")},
-  {key = {"<CR>", "o", "<2-LeftMouse>"}, cb = tree_cb("edit")}, {key = {"<2-RightMouse>", "<C-]>"}, cb = tree_cb("cd")},
-  {key = "<C-v>", cb = tree_cb("vsplit")}, {key = "<C-x>", cb = tree_cb("split")}, {key = "<C-t>", cb = tree_cb("tabnew")},
-  {key = "<", cb = tree_cb("prev_sibling")}, {key = ">", cb = tree_cb("next_sibling")}, {key = "P", cb = tree_cb("parent_node")},
-  {key = "<BS>", cb = tree_cb("close_node")}, {key = "<S-CR>", cb = tree_cb("close_node")}, {key = "<Tab>", cb = tree_cb("preview")},
-  {key = "K", cb = tree_cb("first_sibling")}, {key = "J", cb = tree_cb("last_sibling")}, {key = "I", cb = tree_cb("toggle_ignored")},
-  {key = "H", cb = tree_cb("toggle_dotfiles")}, {key = "R", cb = tree_cb("refresh")}, {key = "r", cb = tree_cb("rename")},
-  {key = "<C-r>", cb = tree_cb("full_rename")}, {key = "y", cb = tree_cb("copy_name")}, {key = "Y", cb = tree_cb("copy_path")},
-  {key = "gy", cb = tree_cb("copy_absolute_path")}, {key = "[c", cb = tree_cb("prev_git_item")}, {key = "]c", cb = tree_cb("next_git_item")},
-  {key = "g?", cb = tree_cb("toggle_help")}
+-- default mappings
+local list = {
+  { key = {"<CR>", "o", "<2-LeftMouse>"}, action = "edit" },
+  { key = {"O"},                          action = "edit_no_picker" },
+  { key = {"<2-RightMouse>", "<C-]>"},    action = "cd" },
+  { key = "<C-v>",                        action = "vsplit" },
+  { key = "<C-x>",                        action = "split" },
+  { key = "<C-t>",                        action = "tabnew" },
+  { key = "<",                            action = "prev_sibling" },
+  { key = ">",                            action = "next_sibling" },
+  { key = "P",                            action = "parent_node" },
+  { key = "<BS>",                         action = "close_node" },
+  { key = "<Tab>",                        action = "preview" },
+  { key = "K",                            action = "first_sibling" },
+  { key = "J",                            action = "last_sibling" },
+  { key = "I",                            action = "toggle_ignored" },
+  { key = "H",                            action = "toggle_dotfiles" },
+  { key = "R",                            action = "refresh" },
+  { key = "a",                            action = "create" },
+  { key = "d",                            action = "remove" },
+  { key = "D",                            action = "trash" },
+  { key = "r",                            action = "rename" },
+  { key = "<C-r>",                        action = "full_rename" },
+  { key = "x",                            action = "cut" },
+  { key = "c",                            action = "copy" },
+  { key = "p",                            action = "paste" },
+  { key = "y",                            action = "copy_name" },
+  { key = "Y",                            action = "copy_path" },
+  { key = "gy",                           action = "copy_absolute_path" },
+  { key = "[c",                           action = "prev_git_item" },
+  { key = "]c",                           action = "next_git_item" },
+  { key = "-",                            action = "dir_up" },
+  { key = "s",                            action = "system_open" },
+  { key = "q",                            action = "close" },
+  { key = "g?",                           action = "toggle_help" },
 }
 
+-- following options are the default
+-- each of these are documented in `:help nvim-tree.OPTION_NAME`
 require'nvim-tree'.setup {
-  diagnostics = {enable = false, icons = {hint = "", info = "", warning = "", error = ""}},
-  -- view = {
-  --   mappings = {
-  --     custom_only = false,
-  --     list = nvim_tree_bindings
-  --   }
-  -- },
-  --   update_cwd          = false,
-  update_to_buf_dir = {enable = true, auto_open = true},
+  disable_netrw        = false,
+  hijack_netrw         = true,
+  open_on_setup        = false,
+  ignore_ft_on_setup   = {},
+  auto_close           = false,
+  auto_reload_on_write = true,
+  open_on_tab          = false,
+  hijack_cursor        = false,
+  update_cwd           = false,
+  hijack_unnamed_buffer_when_opening = true,
+  hijack_directories   = {
+    enable = true,
+    auto_open = true,
+  },
+  diagnostics = {
+    enable = false,
+    icons = {
+      hint = "",
+      info = "",
+      warning = "",
+      error = "",
+    }
+  },
+  update_focused_file = {
+    enable      = false,
+    update_cwd  = false,
+    ignore_list = {}
+  },
+  system_open = {
+    cmd  = nil,
+    args = {}
+  },
   filters = {
     dotfiles = false,
     custom = {
       '.git', 'node_modules', '.cache', '.DS_Store', '.vscode', '__pycache__', '*.pyc', '*~', '.ropeproject', '.hg', '.svn', '_svn', '.tox',
       '.pytest_cache', '.benchmarks', '.venv', 'venv'
     }
+  },
+  git = {
+    enable = true,
+    ignore = true,
+    timeout = 500,
+  },
+  view = {
+    width = 30,
+    height = 30,
+    hide_root_folder = false,
+    side = 'left',
+    auto_resize = false,
+    mappings = {
+      custom_only = false,
+      list = {}
+    },
+    number = false,
+    relativenumber = false,
+    signcolumn = "yes"
+  },
+  trash = {
+    cmd = "trash",
+    require_confirm = true
+  },
+  actions = {
+    change_dir = {
+      global = false,
+    },
+    open_file = {
+      quit_on_open = false,
+    }
   }
 }
 
-map('n', '<leader>n', ':NvimTreeToggle<CR>', {noremap = true, silent = false})
-map('n', '<leader>nr', ':NvimTreeRefresh<CR>', {noremap = true, silent = false})
+key_map('n', '<leader>n', ':NvimTreeToggle<CR>', {noremap = true, silent = true})
+key_map('n', '<leader>nr', ':NvimTreeRefresh<CR>', {noremap = true, silent = false})
