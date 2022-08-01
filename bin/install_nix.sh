8b4e5a6d104dd63b4c967269af3c9aa5d48b2d09
richban <rbanyi@me.com>
2022-08-01 20:59:25 +0100

    Updated nix instalation script

--- bin/install_nix.sh
+++ bin/install_nix.sh
@@ -22,24 +22,29 @@ if [[ ! -f $NIX_CONF_PATH/nix.conf ]] || ! grep "experimental-features" <"$NIX_C
     echo "experimental-features = nix-command flakes" | tee -a "$NIX_CONF_PATH"/nix.conf
 fi
 
+# Install if you want nix-darwin & home-manager CLI's
 if ! grep -q nix-darwin ~/.nix-channels; then
   echo "https://github.com/LnL7/nix-darwin/archive/master.tar.gz darwin" >> ~/.nix-channels
 fi
 export NIX_PATH=darwin=$HOME/.nix-defexpr/channels/darwin:$NIX_PATH
 
 if ! grep -q home-manager ~/.nix-channels; then
-  echo "https://github.com/rycee/home-manager/archive/f5c9303cedd67a57121f0cbe69b585fb74ba82d9.tar.gz home-manager" >> ~/.nix-channels
+  echo "https://github.com/rycee/home-manager/archive/master.tar.gz home-manager" >> ~/.nix-channels
 fi
 export NIX_PATH=home-manager=$HOME/.nix-defexpr/channels/home-manager:$NIX_PATH
 
-nix-channel --update
-export NIX_PATH=darwin-config=$HOME/.dotfiles/system/darwin/default.nix:$NIX_PATH
+# Changing nix-darwin default config path to local git repo
+export NIX_PATH=darwin-config=$HOME/Developer/dotfiles/system/darwin/default.nix:$NIX_PATH
 echo $NIX_PATH
+
+nix-channel --update
+
+# install nix-darwin
 sudo rm /etc/shells /etc/zprofile /etc/zshrc
 nix-build '<darwin>' -A installer --out-link /tmp/nix-darwin && /tmp/nix-darwin/bin/darwin-installer
 
-# Isn't it supposed to do this? It doesn't.
+# courtesy of: https://github.com/burke/b
 rm -rf /run/*
 ln -shf /nix/store/$(ls /nix/store | grep darwin-system- | grep -v drv | head -1) /run/current-system
 
-/run/current-system/sw/bin/darwin-rebuild switch
+# /run/current-system/sw/bin/darwin-rebuild switch
