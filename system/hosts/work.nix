bc14c47eaad7854798c140e51a7237c2a80ea023
richban <rbanyi@me.com>
2022-08-01 20:53:27 +0100

    Added util functions

---
+++ system/hosts/work.nix
@@ -0,0 +1,3 @@
+{ config, lib, pkgs, ... }: {
+    user.name = "richard_banyi";
+}
---
+++ system/primary.nix
@@ -0,0 +1,27 @@
+{ config, lib, options, ... }:
+# module courtesy of https://github.com/kclejeune/system/blob/master/modules/primary.nix
+let inherit (lib) mkAliasDefinitions mkOption types;
+in
+{
+  # Define some aliases for ease of use
+  options = {
+    user = mkOption {
+      description = "Primary user configuration";
+      type = types.attrs;
+      default = { };
+    };
+
+    hm = mkOption {
+      type = types.attrs;
+      default = { };
+    };
+  };
+
+  config = {
+    # hm -> home-manager.users.<primary user>
+    home-manager.users.${config.user.name} = mkAliasDefinitions options.hm;
+
+    # user -> users.users.<primary user>
+    users.users.${config.user.name} = mkAliasDefinitions options.user;
+  };
+}
