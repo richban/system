35875ab551d69fdda6d93c369812f67aad009793
richban <rbanyi@me.com>
2022-11-22 23:37:02 +0000

    Format  autocmd

--- dotfiles/config/nvim/lua/rb/autocmd.lua
+++ dotfiles/config/nvim/lua/rb/autocmd.lua
@@ -3,15 +3,12 @@ vim.cmd([[
     autocmd BufReadPost * if line("'\"") > 1 && line("'\"") <= line("$") | execute "normal! g`\"" | endif
 ]])
 -- Highlight on yank
-vim.api.nvim_exec(
-  [[
+vim.api.nvim_exec([[
     augroup YankHighlight
     autocmd!
     autocmd TextYankPost * silent! lua vim.highlight.on_yank()
     augroup end
-]] ,
-  false
-)
+]], false)
 
 -- vim.api.nvim_exec([[
 --     augroup Format
@@ -20,32 +17,28 @@ vim.api.nvim_exec(
 --     augroup END
 --   ]], false)
 
-vim.api.nvim_exec(
-  [[
+vim.api.nvim_exec([[
   augroup Format
     autocmd!
     autocmd BufWritePre *.ts TSLspOrganize
   augroup END
-]] ,
-  false
-)
+]], false)
 
 -- set markdown FTs
-vim.api.nvim_exec(
-  [[
+vim.api.nvim_exec([[
   augroup SetMarkdownFt
     autocmd!
     autocmd BufNewFile,BufFilePre,BufRead *.markdown,*.mdown,*.mkd,*.mkdn,*.mdwn,*.md,*.MD  set ft=markdown
   augroup end
-]] ,
-  false
-)
+]], false)
 
 -- windows to close with 'q'
-vim.cmd([[autocmd FileType help,qf,fugitive,fugitiveblame,netrw nnoremap <buffer><silent> q :close<CR>]])
+vim.cmd(
+  [[autocmd FileType help,qf,fugitive,fugitiveblame,netrw nnoremap <buffer><silent> q :close<CR>]])
 
 -- windows to close with 'q'
-vim.cmd([[autocmd FileType help,qf,fugitive,fugitiveblame,netrw nnoremap <buffer><silent> q :close<CR>]])
+vim.cmd(
+  [[autocmd FileType help,qf,fugitive,fugitiveblame,netrw nnoremap <buffer><silent> q :close<CR>]])
 
 function vim.fn.TrimWhiteSpace()
   local l = vim.fn.line(".")
