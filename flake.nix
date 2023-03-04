33aa83d46c24e32557d08a9ac3e5cb79c82fedf4
richban <rbanyi@me.com>
2023-03-04 00:51:04 +0000

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -40,6 +40,8 @@
     # comma = { url = github:Shopify/comma; flake = false; };
     # spacebar.url = "github:cmacrae/spacebar/v1.4.0";
     flake-utils.url = "github:numtide/flake-utils";
+    devenv.url = "github:cachix/devenv";
+    treefmt-nix.url = "github:numtide/treefmt-nix";
     devshell = {
       url = "github:numtide/devshell";
       inputs.flake-utils.follows = "flake-utils";
@@ -47,118 +49,150 @@
     };
   };
 
-  outputs = inputs@ { self, nixpkgs, darwin, home-manager, flake-utils, ... }:
-    let
-      inherit (darwin.lib) darwinSystem;
-      inherit (nixpkgs.lib) nixosSystem;
-      inherit (home-manager.lib) homeManagerConfiguration;
-      inherit (flake-utils.lib) eachSystemMap defaultSystems;
-      inherit (builtins) listToAttrs map;
+  outputs = {
+    self,
+    nixpkgs,
+    darwin,
+    home-manager,
+    flake-utils,
+    devenv,
+    ...
+  } @ inputs: let
+    inherit (darwin.lib) darwinSystem;
+    inherit (home-manager.lib) homeManagerConfiguration;
+    inherit (flake-utils.lib) eachSystemMap defaultSystems;
 
-      isDarwin = system: (builtins.elem system nixpkgs.lib.platforms.darwin);
-      homePrefix = system: if isDarwin system then "/Users" else "/home";
+    isDarwin = system: (builtins.elem system nixpkgs.lib.platforms.darwin);
+    homePrefix = system:
+      if isDarwin system
+      then "/Users"
+      else "/home";
 
-      # Overlays is the list of overlays we want to apply from flake inputs.
-      overlays = [
-        inputs.neovim-nightly-overlay.overlay
-        # inputs.spacebar
-      ];
+    # Overlays is the list of overlays we want to apply from flake inputs.
+    overlays = [
+      inputs.neovim-nightly-overlay.overlay
+      # inputs.spacebar
+    ];
 
-      # generate a darwin config
-      mkDarwinConfig =
-        { system ? "x86_64-darwin"
-        , nixpkgs ? inputs.nixpkgs
-        , stable ? inputs.nixos-stable
-        , baseModules ? [ home-manager.darwinModules.home-manager ./system/darwin ]
-        , extraModules ? [ ]
-        }:
-        darwinSystem {
-          inherit system;
-          modules = baseModules ++ extraModules ++ [{ nixpkgs.overlays = overlays; }];
-          specialArgs = { inherit self inputs nixpkgs stable; };
-        };
-
-      # generate a home-manager config for any unix system
-      mkHomeConfig =
-        { username
-        , system ? "aarch64-darwinn"
-        , nixpkgs ? inputs.nixpkgs
-        , stable ? inputs.nixos-stable
-        , baseModules ? [
-            ./system/home-manager
-            {
-              home = {
-                inherit username;
-                homeDirectory = "${homePrefix system}/${username}";
-                sessionVariables = {
-                  NIX_PATH =
-                    "nixpkgs=${nixpkgs}:stable=${stable}\${NIX_PATH:+:}$NIX_PATH";
-                };
-                # stateVersion = "22.05";
-              };
-            }
-          ]
-        , extraModules ? [ ]
-        }:
-        homeManagerConfiguration rec {
-          pkgs = import nixpkgs {
-            inherit system;
-          };
-          extraSpecialArgs = { inherit self inputs nixpkgs stable; };
-          modules = {
-            imports = baseModules ++ extraModules ++ [ ./system/overlays.nix ];
-          };
-        };
-    in
-    {
-
-      darwinConfigurations = {
-        darwinCasperIntel = mkDarwinConfig {
-          system = "x86_64-darwin";
-          extraModules = [ ./system/hosts/personal.nix ];
-        };
-        darwinWorkM1 = mkDarwinConfig {
-          system = "aarch64-darwin";
-          extraModules = [ ./system/hosts/work.nix ];
-        };
+    # generate a darwin config
+    mkDarwinConfig = {
+      system ? "x86_64-darwin",
+      nixpkgs ? inputs.nixpkgs,
+      stable ? inputs.nixos-stable,
+      baseModules ? [home-manager.darwinModules.home-manager ./system/darwin],
+      extraModules ? [],
+    }:
+      darwinSystem {
+        inherit system;
+        modules = baseModules ++ extraModules ++ [{nixpkgs.overlays = overlays;}];
+        specialArgs = {inherit self inputs nixpkgs stable;};
       };
 
-      homeConfigurations = {
-        richbanIntel = mkHomeConfig {
-          username = "richban";
-          system = "x86_64-darwin";
-        };
-        richbanWorkM1 = mkHomeConfig {
-          username = "richard_banyi";
-          system = "aarch64-darwin";
-          extraModules = [ ./system/hosts/work.nix ];
-        };
-      };
-
-      # add a devShell to this flake
-      devShells = eachSystemMap defaultSystems (system:
-        let
-          pkgs = import inputs.nixos-stable {
-            inherit system;
-            overlays = [ inputs.devshell.overlay ];
-          };
-          pyEnv = (pkgs.python3.withPackages
-            (ps: with ps; [ black pylint typer colorama shellingham ]));
-          sysdo = pkgs.writeShellScriptBin "sysdo" ''
-            cd $PRJ_ROOT && ${pyEnv}/bin/python3 bin/do.py $@
-          '';
-        in
+    # generate a home-manager config for any unix system
+    mkHomeConfig = {
+      username,
+      system ? "aarch64-darwinn",
+      nixpkgs ? inputs.nixpkgs,
+      stable ? inputs.nixos-stable,
+      baseModules ? [
+        ./system/home-manager
         {
-          default = pkgs.devshell.mkShell {
-            packages = with pkgs; [ nixfmt pyEnv rnix-lsp stylua treefmt nodePackages.prettier ];
-            commands = [{
-              name = "sysdo";
-              package = sysdo;
-              category = "utilities";
-              help = "perform actions on this repository";
-            }];
+          home = {
+            inherit username;
+            homeDirectory = "${homePrefix system}/${username}";
+            sessionVariables = {
+              NIX_PATH = "nixpkgs=${nixpkgs}:stable=${stable}\${NIX_PATH:+:}$NIX_PATH";
+            };
+            # stateVersion = "22.05";
           };
         }
-      );
+      ],
+      extraModules ? [],
+    }:
+      homeManagerConfiguration rec {
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+        extraSpecialArgs = {inherit self inputs nixpkgs stable;};
+        modules = {
+          imports = baseModules ++ extraModules ++ [./system/overlays.nix];
+        };
+      };
+  in {
+    darwinConfigurations = {
+      darwinCasperIntel = mkDarwinConfig {
+        system = "x86_64-darwin";
+        extraModules = [./system/hosts/personal.nix];
+      };
+      darwinWorkM1 = mkDarwinConfig {
+        system = "aarch64-darwin";
+        extraModules = [./system/hosts/work.nix];
+      };
     };
+
+    homeConfigurations = {
+      richbanIntel = mkHomeConfig {
+        username = "richban";
+        system = "x86_64-darwin";
+      };
+      richbanWorkM1 = mkHomeConfig {
+        username = "richard_banyi";
+        system = "aarch64-darwin";
+        extraModules = [./system/hosts/work.nix];
+      };
+    };
+
+    devShells = eachSystemMap defaultSystems (system: let
+      pkgs = import inputs.nixpkgs {
+        inherit system;
+        overlays = builtins.attrValues self.overlays;
+      };
+    in {
+      default = devenv.lib.mkShell {
+        inherit inputs pkgs;
+        modules = [
+          (import ./devenv.nix)
+        ];
+      };
+    });
+
+    packages = eachSystemMap defaultSystems (
+      system: let
+        pkgs = import inputs.nixpkgs {
+          inherit system;
+          overlays = builtins.attrValues self.overlays;
+        };
+      in rec {
+        pyEnv =
+          pkgs.python3.withPackages
+          (ps: with ps; [black typer colorama shellingham]);
+        sysdo = pkgs.writeScriptBin "sysdo" ''
+          #! ${pyEnv}/bin/python3
+          ${builtins.readFile ./bin/do.py}
+        '';
+      }
+    );
+
+    apps = eachSystemMap defaultSystems (system: rec {
+      sysdo = {
+        type = "app";
+        program = "${self.packages.${system}.sysdo}/bin/sysdo";
+      };
+      default = sysdo;
+      shellHook = ''
+        export PATH=$PATH:${self.packages.${system}.sysdo}/bin/
+      '';
+    });
+
+    overlays = {
+      channels = final: prev: {
+        # expose other channels via overlays
+        unstable = import inputs.unstable {system = prev.system;};
+      };
+      extraPackages = final: prev: {
+        sysdo = self.packages.${prev.system}.sysdo;
+        pyEnv = self.packages.${prev.system}.pyEnv;
+      };
+    };
+  };
 }
