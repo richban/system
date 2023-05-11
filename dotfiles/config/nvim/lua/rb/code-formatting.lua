45615cb8200e966d594106c8e976c4cd1f38b3df
richban <rbanyi@me.com>
2023-05-11 12:14:12 +0100

    Update null-ls

--- dotfiles/config/nvim/lua/rb/code-formatting.lua
+++ dotfiles/config/nvim/lua/rb/code-formatting.lua
@@ -4,12 +4,6 @@ local formatting = null_ls.builtins.formatting
 local diagnostics = null_ls.builtins.diagnostics
 local code_actions = null_ls.builtins.code_actions
 
-local autocmd = require("rb.auto").autocmd
-local autocmd_format = require("rb.auto").autocmd_format
-
-local autocmd_clear = vim.api.nvim_clear_autocmds
-local augroup_highlight = vim.api.nvim_create_augroup("custom-lsp-references", { clear = true })
-
 null_ls.setup({
   sources = {
     -- Nix
@@ -21,7 +15,7 @@ null_ls.setup({
     formatting.black,
     formatting.isort,
     diagnostics.mypy,
-    -- diagnostics.ruff.with({ extra_args = { "--max-line-length=180" } }),
+    diagnostics.ruff.with({ extra_args = { "--max-line-length=180" } }),
     formatting.gofmt,
     formatting.shfmt.with({
       extra_args = { "-i=2" },
@@ -54,15 +48,4 @@ null_ls.setup({
     diagnostics.markdownlint,
     -- diagnostics.misspell,
   },
-  on_attach = function(client, bufnr)
-    if client.server_capabilities.documentFormattingProvider then
-      autocmd_format(false)
-    end
-
-    if client.server_capabilities.documentHighlightProvider then
-      autocmd_clear({ group = augroup_highlight, buffer = bufnr })
-      autocmd({ "CursorHold", augroup_highlight, vim.lsp.buf.document_highlight, buffer = bufnr })
-      autocmd({ "CursorMoved", augroup_highlight, vim.lsp.buf.clear_references, buffer = bufnr })
-    end
-  end,
 })
