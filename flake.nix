a3d332f7cedb9cc8f2a1441fb17f8b058fefd80a
richban <rbanyi@me.com>
2023-12-24 11:36:17 +0100

    Upgrad nix to 23.11

--- flake.nix
+++ flake.nix
@@ -18,9 +18,7 @@
   inputs = {
     # We use the unstable nixpkgs repo for some packages.
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
-
-    # TODO: configure NixOS
-    nixos-stable.url = "github:nixos/nixpkgs/nixos-23.05";
+    nixos-stable.url = "github:nixos/nixpkgs/nixos-23.11";
     nixos-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
 
     # Darwin system management
