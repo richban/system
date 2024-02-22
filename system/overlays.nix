4a3f40f1b383e675d83bce77ee10c2892d2e646a
richban <rbanyi@me.com>
2024-02-22 23:11:04 +0000

    Clean up overlays

--- system/overlays.nix
+++ system/overlays.nix
@@ -5,29 +5,6 @@
       # expose other channels via overlays
       stable = import inputs.nixos-stable {system = prev.system;};
     })
-    # (final: prev: {
-    #   python3 = prev.python3.override {
-    #     packageOverrides = (pfinal: pprev: {
-    #       pyopenssl = pprev.pyopenssl.overrideAttrs (old: {
-    #         meta = old.meta // { broken = false; };
-    #       });
-    #     });
-    #   };
-    #   python39 = prev.python39.override {
-    #     packageOverrides = (pfinal: pprev: {
-    #       pyopenssl = pprev.pyopenssl.overrideAttrs (old: {
-    #         meta = old.meta // { broken = false; };
-    #       });
-    #     });
-    #   };
-    #   python310 = prev.python310.override {
-    #     packageOverrides = (pfinal: pprev: {
-    #       pyopenssl = pprev.pyopenssl.overrideAttrs (old: {
-    #         meta = old.meta // { broken = false; };
-    #       });
-    #     });
-    #   };
-    # })
     (
       final: prev: let
         pkgs = final;
