19ba829d372b0a4bf8cfcd078107083351c8e20f
richban <rbanyi@me.com>
2024-03-10 14:22:39 +0000

    Update copilot

--- dotfiles/config/nvim/lua/rb/copilot.lua
+++ dotfiles/config/nvim/lua/rb/copilot.lua
@@ -5,7 +5,57 @@ local M = {
 }
 
 function M.config()
-  require("copilot").setup({})
+  require("copilot").setup({
+    panel = {
+      enabled = true,
+      auto_refresh = true,
+      keymap = {
+        jump_prev = "[[",
+        jump_next = "]]",
+        accept = "<CR>",
+        refresh = "gr",
+        open = "<M-CR>",
+      },
+      layout = {
+        position = "bottom", -- | top | left | right
+        ratio = 0.4,
+      },
+    },
+    suggestion = {
+      enabled = true,
+      auto_trigger = false,
+      debounce = 75,
+      keymap = {
+        accept = "<M-l>",
+        accept_word = false,
+        accept_line = false,
+        next = "<M-]>",
+        prev = "<M-[>",
+        dismiss = "<C-]>",
+      },
+    },
+    filetypes = {
+      yaml = false,
+      markdown = false,
+      help = false,
+      gitcommit = false,
+      gitrebase = false,
+      hgcommit = false,
+      svn = false,
+      cvs = false,
+      ["."] = false,
+    },
+    copilot_node_command = "node", -- Node.js version must be > 18.x
+    server_opts_overrides = {
+      trace = "verbose",
+      settings = {
+        advanced = {
+          listCount = 10, -- #completions for panel
+          inlineSuggestCount = 3, -- #completions for getCompletions
+        },
+      },
+    },
+  })
 end
 
 function M.test() end
