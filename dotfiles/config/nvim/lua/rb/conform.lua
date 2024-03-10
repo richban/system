bacfa8cceade0e4b45d93b10019551952aaca5d0
richban <rbanyi@me.com>
2024-03-10 18:34:53 +0000

    cqqq
    qtre

---
+++ dotfiles/config/nvim/lua/rb/conform.lua
@@ -0,0 +1,23 @@
+-- formatting
+local M = {
+  "stevearc/conform.nvim",
+  config = function()
+    require("conform").setup({
+      formatters_by_ft = {
+        lua = { "stylua" },
+        -- Conform will run multiple formatters sequentially
+        python = { "isort", "black" },
+        -- Use a sub-list to run only the first available formatter
+        javascript = { { "prettierd", "prettier" } },
+        nix = { "alejandra" },
+      },
+      format_on_save = {
+        -- These options will be passed to conform.format()
+        timeout_ms = 500,
+        lsp_fallback = true,
+      },
+    })
+  end,
+}
+
+return M
