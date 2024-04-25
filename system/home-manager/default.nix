e7dc837fc38ec0924bff01a9f3c0ff0d2dc6010e
richban <rbanyi@me.com>
2024-04-25 13:37:42 +0100

    Tmux catppuccin

--- system/home-manager/default.nix
+++ system/home-manager/default.nix
@@ -80,7 +80,29 @@ in {
       tmuxPlugins.better-mouse-mode
       tmuxPlugins.pain-control
       # tmuxPlugins.vim-tmux-navigator
-      tmuxPlugins.dracula
+      # tmuxPlugins.dracula
+      {
+        plugin = tmuxPlugins.catppuccin;
+        extraConfig = ''
+          set -g @catppuccin_flavour 'mocha'
+          set -g @catppuccin_window_left_separator ""
+          set -g @catppuccin_window_right_separator " "
+          set -g @catppuccin_window_middle_separator " █"
+          set -g @catppuccin_window_number_position "right"
+          set -g @catppuccin_window_default_fill "number"
+          set -g @catppuccin_window_default_text "#W"
+          set -g @catppuccin_window_current_fill "number"
+          set -g @catppuccin_window_current_text "#W#{?window_zoomed_flag,(),}"
+          set -g @catppuccin_status_modules_right "directory meetings date_time"
+          set -g @catppuccin_status_modules_left "session"
+          set -g @catppuccin_status_left_separator  " "
+          set -g @catppuccin_status_right_separator " "
+          set -g @catppuccin_status_right_separator_inverse "no"
+          set -g @catppuccin_status_fill "icon"
+          set -g @catppuccin_status_connect_separator "no"
+          set -g @catppuccin_directory_text "#{b:pane_current_path}"
+        '';
+      }
     ];
     extraConfig = ''
       ${builtins.readFile ../../dotfiles/config/tmux/tmux.conf}
