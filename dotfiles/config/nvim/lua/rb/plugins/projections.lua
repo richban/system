4e663dd3d2f8dabd7387cab6aca6d1eafde158d1
richban <rbanyi@me.com>
2024-05-27 09:56:45 +0100

    Fix projections

--- dotfiles/config/nvim/lua/rb/plugins/projections.lua
+++ dotfiles/config/nvim/lua/rb/plugins/projections.lua
@@ -1,5 +1,9 @@
 return {
   "gnikdroy/projections.nvim",
+  dependencies = {
+    "nvim-telescope/telescope.nvim",
+  },
+  branch = "pre_release",
   config = function()
     require("rb.projections")
   end,
