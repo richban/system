64498b28d03248da50d0c608c19a0d008e8888f4
richban <rbanyi@me.com>
2024-05-27 12:02:03 +0100

    Add new plugins

--- dotfiles/config/nvim/lua/rb/plugins/neogit.lua
+++ dotfiles/config/nvim/lua/rb/plugins/neogit.lua
@@ -13,6 +13,28 @@ local M = {
       },
     })
     vim.api.nvim_set_keymap("n", "<leader>gs", "<cmd>Neogit kind=tab<CR>", { noremap = true, silent = true })
+
+    local function augroup(name)
+      return vim.api.nvim_create_augroup("mnv_" .. name, { clear = true })
+    end
+
+    local group = vim.api.nvim_create_augroup("MyCustomNeogitEvents", { clear = true })
+
+    vim.api.nvim_create_autocmd("User", {
+      pattern = "NeogitPushComplete",
+      group = group,
+      callback = function()
+        require("neogit").close()
+      end,
+    })
+
+    vim.api.nvim_create_autocmd("User", {
+      pattern = "BufWritePost,BufEnter,FocusGained,ShellCmdPost,VimResume",
+      group = augroup("DefaultRefreshEvents"),
+      callback = function()
+        require("neogit").refresh_manually()
+      end,
+    })
   end,
 }
 
