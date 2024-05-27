a0c7ecfa1b2a930457edbe2356e14c95bef3d2a7
richban <rbanyi@me.com>
2024-05-27 09:56:18 +0100

    Add init.lua

--- dotfiles/config/nvim/init.lua
+++ dotfiles/config/nvim/init.lua
@@ -1,7 +1,21 @@
-require("rb.plugins")
-require("rb.utils")
-require("rb.settings")
-require("rb.mappings")
-require("rb.autocmd")
-require("rb.colors")
-require("rb.plugins.init")
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+
+vim.g.mapleader = " "
+
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+require("lazy").setup({ import = "rb/plugins" }, {
+  dev = {
+    path = "~/.local/share/nvim/nix",
+    fallback = false,
+  },
+})
