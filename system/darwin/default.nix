5374048b460c43be25f016e1ef90e96d29b50ff0
richban <rbanyi@me.com>
2025-02-10 23:56:38 +0100

    Update darwin

--- system/darwin/default.nix
+++ system/darwin/default.nix
@@ -3,8 +3,20 @@
     inputs.nix-homebrew.darwinModules.nix-homebrew
     ../common.nix
     ./wm.nix
-    ./core.nix
     ./preferences.nix
     ./brew.nix
   ];
+
+  documentation.enable = true;
+  documentation.doc.enable = false;
+  documentation.info.enable = false;
+  documentation.man.enable = true;
+
+  security.pam.enableSudoTouchIdAuth = true;
+  # Auto upgrade nix package and the daemon service.
+  services.nix-daemon.enable = true;
+
+  # Used for backwards compatibility, please read the changelog before changing.
+  # $ darwin-rebuild changelog
+  system.stateVersion = 5;
 }
