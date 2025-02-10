23735476bd5c9ac222ee61e12c0ec5ca30f56ff3
richban <rbanyi@me.com>
2025-02-10 23:59:42 +0100

    Nix and nixpkgs changes

--- system/nixpkgs.nix
+++ system/nixpkgs.nix
@@ -2,6 +2,9 @@
   config,
   inputs,
   self,
+  platform,
+  lib,
+  username,
   ...
 }: {
   nixpkgs = {
@@ -11,40 +14,29 @@
       allowUnfree = true;
       allowBroken = false;
     };
-  };
-
-  nix = {
-    # Enable flakes.
-    extraOptions = ''
-      keep-outputs = true
-      keep-derivations = true
-      experimental-features = nix-command flakes
-    '';
-
-    # Garbage collection.
-    gc = {
-      automatic = true;
-      options = "--delete-older-than 14d";
+    hostPlatform = lib.mkDefault "${platform}";
+  };  
+  nix =
+    let
+      flakeInputs = lib.filterAttrs (_: lib.isType "flake") inputs;
+    in
+    {
+      settings = {
+        experimental-features = "flakes nix-command";
+        # Disable global registry
+        flake-registry = "";
+        # Workaround for https://github.com/NixOS/nix/issues/9574
+        nix-path = config.nix.nixPath;
+        trusted-users = [
+          "root"
+          "${username}"
+        ];
+        warn-dirty = false;
+      };
+      # Disable channels
+      channel.enable = false;
+      # Make flake registry and nix path match flake inputs
+      registry = lib.mapAttrs (_: flake: { inherit flake; }) flakeInputs;
+      nixPath = lib.mapAttrsToList (n: _: "${n}=flake:${n}") flakeInputs;
     };
-
-    settings = {
-      max-jobs      = 8;
-      trusted-users = ["${config.user.name}" "root" "@admin" "@sudo" "@wheel"];
-      # Optionally add trusted-substituters and trusted-public-keys:
-      # trusted-substituters = [
-      #   "https://cache.nixos.org"
-      #   "https://nix-community.cachix.org"
-      #   "https://richban.cachix.org"
-      # ];
-      # trusted-public-keys = [
-      #   "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
-      #   "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-      #   "richban.cachix.org-1:b22iBPRwWfvVe1ldyn3ca1JRw0OEzzf3jrurGJQN3LA="
-      # ];
-    };
-
-    registry = {
-      home-manager.flake = inputs.home-manager;
-    };
-  };
 }
