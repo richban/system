bcb7910969918b34768b4d6f0989126e5d80034b
richban <rbanyi@me.com>
2025-07-16 15:00:36 +0200

    Remove deprecated Neovim diagnostic configuration from lspsaga.lua

--- dotfiles/config/nvim/lua/rb/lsp/lspsaga.lua
+++ dotfiles/config/nvim/lua/rb/lsp/lspsaga.lua
@@ -109,13 +109,3 @@ for type, icon in pairs(signs) do
   local hl = "DiagnosticSign" .. type
   vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
 end
-
-vim.diagnostic.config({
-  signs = true,
-  update_in_insert = false,
-  underline = true,
-  severity_sort = true,
-  virtual_text = {
-    source = true,
-  },
-})
