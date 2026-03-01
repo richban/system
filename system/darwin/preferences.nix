6daf7091ce71f4229bf3b22d85a2855a1eebc027
richban <rbanyi@me.com>
2026-03-01 18:54:32 +0100

    feat: Update persistent applications in preferences.nix, replacing Launchpad with System Settings and adding new apps including Ghostty, Antigravity, and GitHub Desktop.

--- system/darwin/preferences.nix
+++ system/darwin/preferences.nix
@@ -92,21 +92,23 @@
       mru-spaces = false;
       minimize-to-application = true;
       persistent-apps = [
-        # "/System/Applications/Launchpad.app"
+        "/System/Applications/System Settings.app"
         "/Applications/Arc.app"
         "/System/Applications/Messages.app"
         "/System/Applications/FaceTime.app"
         "/System/Applications/Mail.app"
         "/Applications/WeChat.app"
+        "/Applications/WhatsApp.app"
         "/Applications/Discord.app"
         "/System/Applications/Calendar.app"
         "/System/Applications/Notes.app"
         "/System/Applications/Freeform.app"
         "/Applications/Obsidian.app"
         "/Applications/Notion.app"
-        # "/Applications/TickTick.app"
-        "/Applications/Alacritty.app"
+        "/Applications/Ghostty.app"
         "/Applications/Cursor.app"
+        "/Applications/Antigravity.app"
+        "/Applications/GitHub Desktop.app"
       ];
     };
 
