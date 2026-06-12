b9247e7afad67ee6fce2de78731e2b27e3d1330c
richban <rbanyi@me.com>
2026-06-12 09:22:15 +0200

    Make brew packagse profile agnostic

--- system/darwin/brew.nix
+++ system/darwin/brew.nix
@@ -7,62 +7,7 @@
     onActivation = {
       autoUpdate = false;
       upgrade = false;
-      cleanup = "uninstall";
+      cleanup = "none";
     };
-    brews = [
-      "gromgit/brewtils/taproom"
-      "terminal-notifier"
-      "rilldata/tap/rill"
-      "felixkratz/formulae/borders"
-      "gnuplot"
-      "graphviz"
-      "databricks"
-      "dust"
-      "btop"
-      "chafa"
-      "csvlens"
-      "PeonPing/tap/peon-ping"
-    ];
-    taps = [
-      "1password/tap"
-      "homebrew/bundle"
-      "homebrew/services"
-      "rilldata/tap"
-      "nikitabobko/tap"
-      "FelixKratz/formulae"
-      "databricks/tap"
-      "AlexsJones/llmfit"
-      "gromgit/brewtils"
-      {
-        name = "chmouel/lazyworktree";
-        clone_target = "https://github.com/chmouel/lazyworktree.git";
-      }
-    ];
-    casks = [
-      # "alacritty"
-      "arc"
-      "firefox"
-      "1password"
-      "1password-cli"
-      # "docker-desktop"
-      "hammerspoon"
-      "obsidian"
-      "raycast"
-      "discord"
-      "notion"
-      "slack"
-      "wechat"
-      # "ticktick"
-      "github"
-      "karabiner-elements"
-      "zoom"
-      "whatsapp"
-      "microsoft-teams"
-      "aerospace"
-      # "godot"
-      "ghostty"
-      "orbstack"
-      "chmouel/lazyworktree/lazyworktree"
-    ];
   };
 }
