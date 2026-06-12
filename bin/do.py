c319770695288534d6dbbd7b20ee991f46097bd8
richban <rbanyi@me.com>
2026-06-12 09:18:59 +0200

    refactor: remove disk_setup

--- bin/do.py
+++ bin/do.py
@@ -144,7 +144,6 @@ def bootstrap(
         )
         raise typer.Abort()
     elif cfg == FlakeOutputs.DARWIN:
-        disk_setup()
         flake = f"{bootstrap_flake}#{cfg.value}.{host}.config.system.build.toplevel"
         run_cmd(["nix", "build", flake] + flags)
         run_cmd(
@@ -219,31 +218,6 @@ def clean(
     ),
 ):
     run_cmd(f"find . -type l -maxdepth 1 -name {filename} -exec rm {{}} +".split())
-
-
-@app.command(
-    hidden=PLATFORM != FlakeOutputs.DARWIN,
-    help="configure disk setup for nix-darwin",
-)
-def disk_setup():
-    synthetic_conf = "/etc/synthetic.conf"
-    apfs_util = "/System/Library/Filesystems/apfs.fs/Contents/Resources/apfs.util"
-    run_mount_configured = False
-    with open(synthetic_conf, "r") as f:
-        run_mount_configured = any(
-            "run\tprivate/var/run" == l.strip() for l in f.readlines()
-        )
-    if not run_mount_configured:
-        typer.secho(f"setting up {synthetic_conf}", fg=Colors.INFO.value)
-        run_cmd(
-            f"echo 'run\\tprivate/var/run' | sudo tee -a {synthetic_conf}".split(),
-            shell=True,
-        )
-        run_cmd(f"sudo {apfs_util} -t".split())
-    if not os.path.islink("/run"):
-        typer.secho("linking /run directory", fg=Colors.INFO.value)
-        run_cmd("sudo ln -sfn private/var/run /run".split())
-    typer.secho("disk setup complete", fg=Colors.SUCCESS.value)
 
 
 @app.command(
